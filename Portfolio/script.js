4971496b2493e36b7e2e7b64a8ee339fc59133c6
AntBezr <sosleepyanton@gmail.com>
2023-03-16 21:54:29 +0200

    Cleaning and last check of the code. Deleting balconies

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,9 +1,11 @@
 const backButton = document.querySelector('#backToTop')
 const header = document.querySelector('header')
 const menu = document.querySelector('.menu')
+const logoName = document.querySelector('.menu h5')
 const mobButton = document.querySelector('.mobile')
 const nav = document.querySelector('nav ul')
 const menuItems = document.querySelectorAll('ul li a')
+
 const path = window.location.pathname;
 const page = path.substring(path.lastIndexOf('/') + 1);
 
@@ -13,15 +15,20 @@ const scrollFunction = () => {
     if (
       document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
       backButton.style.display = 'block';
+      mobButton.style.color = "var(--blackText)"
       menu.style.background = '#faebd7e6';
+      logoName.style.color = "var(--blackText)"
       menuItems.forEach(item => {
-        item.style.color = "black"
+        item.style.color = "var(--blackText)"
       });
     } else {
       backButton.style.display = 'none';
       menu.style.background = 'transparent';
+      logoName.style.color = "var(--primary-color)"
+      mobButton.style.color = "var(--primary-color)"
       menuItems.forEach(item => {
         item.style.color = "var(--primary-color)"
+
       });
     }
   }
